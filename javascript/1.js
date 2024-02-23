b4dfa845407caa409593e959a757309160b2a3f7
jorge <stevenhijo@hotmail.com>
2024-02-23 00:08:41 -0500

    creacion carrusel guitarra

--- javascript/1.js
+++ javascript/1.js
@@ -52,4 +52,36 @@ function toggleAnswer(answerId) {
       })
   })
   
- 
+ // carrusel clase de guitarra
+
+ let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide1");
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
