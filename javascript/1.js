4bc9223fd5703e5715b627d3563da01dc8c2293b
jorge <stevenhijo@hotmail.com>
2024-02-23 14:17:37 -0500

    formacion tecnica vocal

--- javascript/1.js
+++ javascript/1.js
@@ -85,3 +85,37 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+// carrusel clase de tecnica vocal
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function moveSlide2(n) {
+    showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide2");
+    const dots = document.getElementsByClassName("dot2");
+
+    if (n > slides.length) {
+        slideIndex2 = 1;
+    }
+    if (n < 1) {
+        slideIndex2 = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex2 - 1].style.display = "block";
+    dots[slideIndex2 - 1].className += " active";
+}
