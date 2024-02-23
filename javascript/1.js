1b614c99197bfdfb18ab1426535cfe1a496dd4fd
jorge <stevenhijo@hotmail.com>
2024-02-23 15:50:10 -0500

    clase preorquesta

--- javascript/1.js
+++ javascript/1.js
@@ -119,3 +119,39 @@ function showSlides2(n) {
     slides[slideIndex2 - 1].style.display = "block";
     dots[slideIndex2 - 1].className += " active";
 }
+
+// carrusel clase de PREORQUESTA
+
+let slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+function moveSlide3(n) {
+    showSlides3(slideIndex3 += n);
+}
+
+function currentSlide3(n) {
+    showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide3");
+    const dots = document.getElementsByClassName("dot3");
+
+    if (n > slides.length) {
+        slideIndex3 = 1;
+    }
+    if (n < 1) {
+        slideIndex3 = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex3 - 1].style.display = "block";
+    dots[slideIndex3 - 1].className += " active";
+}
+
+
